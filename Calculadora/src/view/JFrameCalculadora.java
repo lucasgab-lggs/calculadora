37f2b2943752b1a88c17aad3ea6babc5257ee6d6
Lucas Gabriel <lucasgab.lggs@gmail.com>
2021-05-03 14:17:13 -0300

    Update 0.6 - Code and design improvement.

--- Calculadora/src/view/JFrameCalculadora.java
+++ Calculadora/src/view/JFrameCalculadora.java
@@ -40,7 +40,7 @@ public class JFrameCalculadora extends JFrame {
 	 * Create the frame.
 	 */
 	public JFrameCalculadora() {
-		setTitle("Calculadora - Vers\u00E3o 0.5");
+		setTitle("Calculadora - Vers\u00E3o 0.6");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 310, 320);
 		contentPane = new JPanel();
@@ -56,88 +56,16 @@ public class JFrameCalculadora extends JFrame {
 		txtNumber.setBounds(7, 11, 262, 60);
 		txtNumber.setEditable(false);
 		txtNumber.setColumns(10);
-
-		JButton botao_7 = new JButton("7");
-		botao_7.setBounds(7, 82, 58, 32);
-		botao_7.addActionListener(new ActionListener() {
+		
+		JButton botao_0 = new JButton("0");
+		botao_0.setBounds(7, 211, 58, 32);
+		botao_0.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				limpar();
-				txtNumber.setText(txtNumber.getText() + "7");
+				txtNumber.setText(txtNumber.getText() + "0");
 			}
 		});
-		botao_7.setFont(new Font("Tahoma", Font.PLAIN, 10));
-
-		JButton botao_8 = new JButton("8");
-		botao_8.setBounds(75, 82, 58, 32);
-		botao_8.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				limpar();
-				txtNumber.setText(txtNumber.getText() + "8");
-			}
-		});
-		botao_8.setFont(new Font("Tahoma", Font.PLAIN, 10));
-
-		JButton botao_9 = new JButton("9");
-		botao_9.setBounds(143, 82, 58, 32);
-		botao_9.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				limpar();
-				txtNumber.setText(txtNumber.getText() + "9");
-			}
-		});
-		botao_9.setFont(new Font("Tahoma", Font.PLAIN, 10));
-
-		JButton botao_mais = new JButton("+");
-		botao_mais.setBounds(211, 82, 58, 32);
-		botao_mais.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				valor1 = Double.parseDouble(txtNumber.getText());
-				operacao = Operacao.SOMA;
-				operar();
-			}
-		});
-		botao_mais.setFont(new Font("Tahoma", Font.PLAIN, 10));
-
-		JButton botao_4 = new JButton("4");
-		botao_4.setBounds(7, 125, 58, 32);
-		botao_4.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				limpar();
-				txtNumber.setText(txtNumber.getText() + "4");
-			}
-		});
-		botao_4.setFont(new Font("Tahoma", Font.PLAIN, 10));
-
-		JButton botao_5 = new JButton("5");
-		botao_5.setBounds(75, 125, 58, 32);
-		botao_5.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				limpar();
-				txtNumber.setText(txtNumber.getText() + "5");
-			}
-		});
-		botao_5.setFont(new Font("Tahoma", Font.PLAIN, 10));
-
-		JButton botao_6 = new JButton("6");
-		botao_6.setBounds(143, 125, 58, 32);
-		botao_6.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				limpar();
-				txtNumber.setText(txtNumber.getText() + "6");
-			}
-		});
-		botao_6.setFont(new Font("Tahoma", Font.PLAIN, 10));
-
-		JButton botao_menos = new JButton("-");
-		botao_menos.setBounds(211, 125, 58, 32);
-		botao_menos.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				valor1 = Double.parseDouble(txtNumber.getText());
-				operacao = Operacao.SUBTRACAO;
-				operar();
-			}
-		});
-		botao_menos.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		botao_0.setFont(new Font("Tahoma", Font.PLAIN, 10));
 
 		JButton botao_1 = new JButton("1");
 		botao_1.setBounds(7, 168, 58, 32);
@@ -168,17 +96,67 @@ public class JFrameCalculadora extends JFrame {
 			}
 		});
 		botao_3.setFont(new Font("Tahoma", Font.PLAIN, 10));
-
-		JButton botao_0 = new JButton("0");
-		botao_0.setBounds(7, 211, 58, 32);
-		botao_0.addActionListener(new ActionListener() {
+		
+		JButton botao_4 = new JButton("4");
+		botao_4.setBounds(7, 125, 58, 32);
+		botao_4.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				limpar();
-				txtNumber.setText(txtNumber.getText() + "0");
+				txtNumber.setText(txtNumber.getText() + "4");
 			}
 		});
-		botao_0.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		botao_4.setFont(new Font("Tahoma", Font.PLAIN, 10));
 
+		JButton botao_5 = new JButton("5");
+		botao_5.setBounds(75, 125, 58, 32);
+		botao_5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				limpar();
+				txtNumber.setText(txtNumber.getText() + "5");
+			}
+		});
+		botao_5.setFont(new Font("Tahoma", Font.PLAIN, 10));
+
+		JButton botao_6 = new JButton("6");
+		botao_6.setBounds(143, 125, 58, 32);
+		botao_6.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				limpar();
+				txtNumber.setText(txtNumber.getText() + "6");
+			}
+		});
+		botao_6.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		
+		JButton botao_7 = new JButton("7");
+		botao_7.setBounds(7, 82, 58, 32);
+		botao_7.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				limpar();
+				txtNumber.setText(txtNumber.getText() + "7");
+			}
+		});
+		botao_7.setFont(new Font("Tahoma", Font.PLAIN, 10));
+
+		JButton botao_8 = new JButton("8");
+		botao_8.setBounds(75, 82, 58, 32);
+		botao_8.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				limpar();
+				txtNumber.setText(txtNumber.getText() + "8");
+			}
+		});
+		botao_8.setFont(new Font("Tahoma", Font.PLAIN, 10));
+
+		JButton botao_9 = new JButton("9");
+		botao_9.setBounds(143, 82, 58, 32);
+		botao_9.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				limpar();
+				txtNumber.setText(txtNumber.getText() + "9");
+			}
+		});
+		botao_9.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		
 		JButton botao_limpar = new JButton("C");
 		botao_limpar.setBounds(75, 211, 58, 32);
 		botao_limpar.addActionListener(new ActionListener() {
@@ -191,26 +169,29 @@ public class JFrameCalculadora extends JFrame {
 		});
 		botao_limpar.setFont(new Font("Tahoma", Font.PLAIN, 10));
 
-		JButton botao_igual = new JButton("=");
-		botao_igual.setBounds(143, 211, 58, 32);
-		botao_igual.addActionListener(new ActionListener() {
+		JButton botao_mais = new JButton("+");
+		botao_mais.setBounds(211, 82, 58, 32);
+		botao_mais.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				auxiliar = false;
-				valor2 = Double.parseDouble(txtNumber.getText());
-				if (validaDivisao()) {
-					operar();
-				} else {
-					txtNumber.setText("Impossível");
-					limpar = true;
-				}
-				operacao = Operacao.NENHUMA;
-				valor1 = 0;
-				valor2 = 0;
+				valor1 = Double.parseDouble(txtNumber.getText());
+				operacao = Operacao.SOMA;
+				operar();
 			}
 		});
-		botao_igual.setFont(new Font("Dialog", Font.PLAIN, 10));
+		botao_mais.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		
+		JButton botao_menos = new JButton("-");
+		botao_menos.setBounds(211, 125, 58, 32);
+		botao_menos.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				valor1 = Double.parseDouble(txtNumber.getText());
+				operacao = Operacao.SUBTRACAO;
+				operar();
+			}
+		});
+		botao_menos.setFont(new Font("Tahoma", Font.PLAIN, 10));
 
-		JButton botao_mult = new JButton("*");
+		JButton botao_mult = new JButton("X");
 		botao_mult.setBounds(211, 211, 58, 32);
 		botao_mult.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -233,6 +214,26 @@ public class JFrameCalculadora extends JFrame {
 			}
 		});
 		botao_divisao.setFont(new Font("Tahoma", Font.PLAIN, 10));
+
+		JButton botao_igual = new JButton("=");
+		botao_igual.setBounds(143, 211, 58, 32);
+		botao_igual.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				auxiliar = false;
+				valor2 = Double.parseDouble(txtNumber.getText());
+				if (validaDivisao()) {
+					operar();
+				} else {
+					txtNumber.setText("Impossï¿½vel");
+					limpar = true;
+				}
+				operacao = Operacao.NENHUMA;
+				valor1 = 0;
+				valor2 = 0;
+			}
+		});
+		botao_igual.setFont(new Font("Dialog", Font.PLAIN, 10));
+
 		panel.setLayout(null);
 		panel.add(txtNumber);
 		panel.add(botao_1);
@@ -268,7 +269,7 @@ public class JFrameCalculadora extends JFrame {
 			if (validaDivisao() && auxiliar == false) {
 				valor1 = valor1 / valor2;
 			} else {
-				txtNumber.setText("Impossível");
+				txtNumber.setText("Impossï¿½vel");
 			}
 			break;
 		case MULTIPLICACAO:
